e7774597c4cf9f2d708b615c00ad9c876cca05d4
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-05-26 16:29:37 -0300

    Creating Task Controller

---
+++ src/main/java/br/com/cadilam/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.cadilam.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    TaskModel findByTitle(String name);
+}
---
+++ src/main/java/br/com/cadilam/todolist/task/TaskController.java
@@ -0,0 +1,31 @@
+package br.com.cadilam.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+
+        var task = taskRepository.findByTitle(taskModel.getTitle());
+
+        if (task != null) {
+            return ResponseEntity.status(400).body("Task already.");
+        }
+
+        var taskCreated = this.taskRepository.save(taskModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+}
