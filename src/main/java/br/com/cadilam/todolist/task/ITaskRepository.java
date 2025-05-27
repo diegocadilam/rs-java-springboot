af39d0cf2128d4979d9b63e41c11ebb63c994df1
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-05-27 10:34:57 -0300

    Creating method for list all tasks of specific user

--- src/main/java/br/com/cadilam/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/cadilam/todolist/task/ITaskRepository.java
@@ -3,7 +3,10 @@ package br.com.cadilam.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
     TaskModel findByTitle(String name);
+
+    List<TaskModel> findByIdUser(UUID idUser);
 }
--- src/main/java/br/com/cadilam/todolist/task/TaskController.java
+++ src/main/java/br/com/cadilam/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.cadilam.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,32 +25,40 @@ public class TaskController {
     @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
-        var task = taskRepository.findByTitle(taskModel.getTitle());
+        // var task = taskRepository.findByTitle(taskModel.getTitle());
 
-        if (task != null) {
-            return ResponseEntity.status(400).body("Task already.");
-        }
+        // if (task != null) {
+        // return ResponseEntity.status(400).body("Task already.");
+        // }
 
-        // taskModel.setId(null);
+        taskModel.setIdUser((UUID) request.getAttribute("idUser"));
 
         System.out.println("Passou pela Controller");
 
         var currentDate = LocalDateTime.now();
 
-        if(currentDate.isAfter(taskModel.getStartDate()) || currentDate.isAfter(taskModel.getEndtDate())){
-            ResponseEntity.status(400, "A data de inicio / datade término dever ser maior do que a data atual.")
+        if (currentDate.isAfter(taskModel.getStartDate()) || currentDate.isAfter(taskModel.getEndtDate())) {
+            return ResponseEntity.status(400)
+                    .body("A data de inicio / datade término dever ser maior do que a data atual.");
         }
 
-        if(currentDate.isAfter(taskModel.getStartDate()) || currentDate.isAfter(taskModel.getEndtDate())){
-            ResponseEntity.status(400).body("A data de inicio / datade término dever ser maior do que a data atual.")
+        if (currentDate.isAfter(taskModel.getStartDate()) || currentDate.isAfter(taskModel.getEndtDate())) {
+            return ResponseEntity.status(400)
+                    .body("A data de inicio / datade término dever ser maior do que a data atual.");
         }
 
-        if(taskModel.getStartDate().isAfter(taskModel.getEndtDate()){
-            ResponseEntity.status(400).body("A data de inicio deve ser menor do que a data de termino.")
+        if (taskModel.getStartDate().isAfter(taskModel.getEndtDate())) {
+            return ResponseEntity.status(400).body("A data de inicio deve ser menor do que a data de termino.");
         }
 
         var taskCreated = this.taskRepository.save(taskModel);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var list = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
+        return list;
     }
 }
