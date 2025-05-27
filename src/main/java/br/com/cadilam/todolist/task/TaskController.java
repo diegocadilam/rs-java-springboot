40cdc93412d9ab6fae0344652bde79ccd74aa52d
diegocadilam <diego.rodrigues@cadilam.com.br>
2025-05-27 11:57:14 -0300

    Creating partil update tasks

--- src/main/java/br/com/cadilam/todolist/task/TaskController.java
+++ src/main/java/br/com/cadilam/todolist/task/TaskController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cadilam.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -60,5 +63,14 @@ public class TaskController {
     public List<TaskModel> list(HttpServletRequest request) {
         var list = this.taskRepository.findByIdUser((UUID) request.getAttribute("idUser"));
         return list;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
